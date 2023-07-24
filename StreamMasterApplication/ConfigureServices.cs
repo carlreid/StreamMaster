eef2456b71f287e2906787cc0e23744c767d4c9e
Carl Reid <kontakt@carlreid.dk>
2023-07-24 20:35:54 +0200

    Use yielding/tasks and HttpClientFactory
    Might speed some things up, though untested...

--- StreamMasterApplication/ConfigureServices.cs
+++ StreamMasterApplication/ConfigureServices.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using StreamMasterApplication.Common.Behaviours;
+using StreamMasterApplication.Icons.Commands;
 
 namespace StreamMasterApplication;
 
@@ -13,6 +14,11 @@ public static class ConfigureServices
         _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
         _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         _ = services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+        _ = services.AddHttpClient<IRequestHandler<CacheIconsFromEPGsRequest, bool>>((httpClient) =>
+        {
+            string userAgentString = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.57";
+            httpClient.DefaultRequestHeaders.Add("User-Agent", userAgentString);
+        });
 
         return services;
     }
--- StreamMasterApplication/Icons/Commands/CacheIconsFromProgrammesRequest.cs
+++ StreamMasterApplication/Icons/Commands/CacheIconsFromProgrammesRequest.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 using StreamMasterDomain.Dto;
-
+using StreamMasterDomain.Extensions;
 using System.Text;
 using System.Text.Json;
 using System.Web;
@@ -27,19 +27,22 @@ public class CacheIconsFromEPGsRequestHandler : IRequestHandler<CacheIconsFromEP
     private readonly IMapper _mapper;
     private readonly IMemoryCache _memoryCache;
     private readonly ISender _sender;
+    private readonly HttpClient _httpClient;
 
     public CacheIconsFromEPGsRequestHandler(
         ILogger<CacheIconsFromEPGsRequestHandler> logger,
         IMemoryCache memoryCache,
-          IMapper mapper,
-
-         IAppDbContext context, ISender sender)
+        IMapper mapper,
+        IAppDbContext context,
+        ISender sender,
+        HttpClient httpClient)
     {
         _memoryCache = memoryCache;
         _logger = logger;
         _mapper = mapper;
         _context = context;
         _sender = sender;
+        _httpClient = httpClient;
     }
 
     public async Task<bool> Handle(CacheIconsFromEPGsRequest command, CancellationToken cancellationToken)
@@ -50,119 +53,109 @@ public class CacheIconsFromEPGsRequestHandler : IRequestHandler<CacheIconsFromEP
             return false;
         }
 
-        var icons = new List<string>();
+        await Task.WhenAll(_context.EPGFiles.Select(epgFile => ProcessEPGFile(epgFile, _setting, cancellationToken)));
 
-        var epgFiles = _context.EPGFiles.ToList();
-        foreach (var epg in epgFiles)
+        var icons = _memoryCache.Programmes()
+            .Where(p => p.Icon is not null && p.Icon.Any(i => i.Src is not null))
+            .SelectMany(p => p.Icon!.Where(i => i.Src is not null).Select(i => i.Src!))
+            .Distinct();
+
+        if (!icons.Any())
         {
-            var tv = await epg.GetTV();
-            if (tv == null)
-            {
-                continue;
-            }
-
-            icons = tv.Channel
-                .Where(a => a is not null && a.Icon is not null && !string.IsNullOrEmpty(a.Icon.Src))
-                .Select(a => a.Icon.Src)
-                .ToList();
-
-            if (!icons.Any()) { continue; }
-
-            await WorkOnIcons(FileDefinitions.Icon, icons, _setting, cancellationToken).ConfigureAwait(false);
+            return false;
         }
 
-        IEnumerable<StreamMasterDomain.Entities.EPG.Programme> _programmes = _memoryCache.Programmes();
-
-        icons = _programmes
-           .Where(a => a is not null && a.Icon is not null && a.Icon.Count > 0 && a.Icon.Any(a => a.Src is not null))
-           .SelectMany(a => a.Icon.Where(a => a.Src is not null).Select(a => a.Src))
-           .Distinct().ToList();
-
-        var test = icons.FirstOrDefault(a => a.Contains("deba6af644347122056ec73f6b885215ff4534230b214addfc795ae7db60c38f"));
-
-        if (!icons.Any()) { return false; }
-
-        await WorkOnIcons(FileDefinitions.ProgrammeIcon, icons, _setting, cancellationToken).ConfigureAwait(false);
+        await ProcessIcons(FileDefinitions.ProgrammeIcon, icons, _setting, cancellationToken);
 
         return true;
     }
 
-    private async Task<bool> WorkOnIcons(FileDefinition fd, List<string> icons, SettingDto setting, CancellationToken cancellationToken)
+    private async Task ProcessEPGFile(EPGFile epg, SettingDto setting, CancellationToken cancellationToken)
     {
-        int count = 0;
+        var tv = await epg.GetTV().ConfigureAwait(false);
+        if (tv == null)
+        {
+            return;
+        }
+
+        var icons = tv.Channel
+            .Where(tvChannel => tvChannel is not null && tvChannel.Icon is not null && !string.IsNullOrEmpty(tvChannel.Icon.Src))
+            .Select(a => a.Icon!.Src!)
+            .ToList();
+
+        if (!icons.Any())
+        {
+            return;
+        }
+
+        await ProcessIcons(FileDefinitions.Icon, icons, setting, cancellationToken).ConfigureAwait(false);
+    }
+
+    private async Task ProcessIcons(FileDefinition fd, IEnumerable<string> icons, SettingDto setting, CancellationToken cancellationToken)
+    {
+        const int BatchSize = 100;
         var isNew = false;
         string token = "";
+        var batchCount = 0;
+        var batches = icons
+            .Where(icon => icon is not null)
+            .Batch(BatchSize);
 
-        foreach (string? icon in icons)
+        foreach (var batch in batches)
         {
-            if (icon is null)
+            if (cancellationToken.IsCancellationRequested)
             {
-                continue;
+                return;
             }
 
-            if (cancellationToken.IsCancellationRequested) { return false; }
+            var tasks = batch
+                .Select(icon => ProcessIcon(icon!, token, setting, fd, cancellationToken));
 
-            ++count;
-            if (count % 100 == 0)
-            {
-                Console.WriteLine($"CacheIconsFromEPGs {count} of {icons.Count}");
-            }
+            await Task.WhenAll(tasks).ConfigureAwait(false);
 
-            //if (icon.Contains("deba6af644347122056ec73f6b885215ff4534230b214addfc795ae7db60c38f"))
-            //{
-            //    var aaa = 1;
-            //}
+            isNew |= tasks.Any(t => t.Result);
 
-            string source = HttpUtility.UrlDecode(icon);
-            string tocheck = source;
-
-            if (tocheck.ToLower().StartsWith("https://json.schedulesdirect.org/20141201/image/"))
-            {
-                if (token == "")
-                {
-                    using HttpClient httpClient = new();
-
-                    SDGetTokenRequest data = new()
-                    {
-                        username = setting.SDUserName,
-                        password = setting.SDPassword
-                    };
-
-                    string jsonString = JsonSerializer.Serialize(data);
-                    StringContent content = new(jsonString, Encoding.UTF8, "application/json");
-                    string userAgentString = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.57";
-
-                    httpClient.DefaultRequestHeaders.Add("User-Agent", userAgentString);
-                    using HttpResponseMessage response = await httpClient.PostAsync("https://json.schedulesdirect.org/20141201/token", content, cancellationToken).ConfigureAwait(false);
-                    try
-                    {
-                        _ = response.EnsureSuccessStatusCode();
-                        string responseString = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
-                        SDGetToken? result = JsonSerializer.Deserialize<SDGetToken>(responseString);
-                        if (result == null || string.IsNullOrEmpty(result.token))
-                        {
-                            continue;
-                        }
-                        token = result.token;
-                    }
-                    catch (HttpRequestException ex)
-                    {
-                        //_logger.LogCritical(ex, "Error while retieving icon");
-                    }
-                    if (string.IsNullOrEmpty(token))
-                    {
-                        continue;
-                    }
-                }
-                string name = Path.GetFileNameWithoutExtension(tocheck);
-                (_, isNew) = await IconHelper.AddIcon(tocheck, "?token=" + token, name, _context, _mapper, setting, fd, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                string name = Path.GetFileNameWithoutExtension(tocheck);
-                (_, isNew) = await IconHelper.AddIcon(tocheck, null, name, _context, _mapper, setting, fd, cancellationToken).ConfigureAwait(false);
-            }
+            batchCount++;
+            _logger.LogDebug($"CacheIconsFromEPGs Batch {batchCount} of {batches.Count()}");
         }
+    }
+
+    private async Task<bool> ProcessIcon(string icon, string token, SettingDto setting, FileDefinition fd, CancellationToken cancellationToken)
+    {
+        string decodedIconUrl = HttpUtility.UrlDecode(icon);
+        string? queryString = null;
+
+        if (decodedIconUrl.ToLower().StartsWith("https://json.schedulesdirect.org/20141201/image/"))
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                var request = new SDGetTokenRequest
+                {
+                    username = setting.SDUserName,
+                    password = setting.SDPassword
+                };
+
+                using var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+
+                using var getTokenResponse = await _httpClient.PostAsync("https://json.schedulesdirect.org/20141201/token", content, cancellationToken).ConfigureAwait(false);
+                getTokenResponse.EnsureSuccessStatusCode();
+
+                var getTokenResponseContentStream = await getTokenResponse.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
+                var getTokenResponseContent = JsonSerializer.Deserialize<SDGetToken>(getTokenResponseContentStream);
+
+                if (getTokenResponseContent == null || string.IsNullOrEmpty(getTokenResponseContent.token))
+                {
+                    return false;
+                }
+
+                token = getTokenResponseContent.token;
+            }
+
+            queryString = "?token=" + token;
+        }
+
+        string name = Path.GetFileNameWithoutExtension(decodedIconUrl);
+        var (_, isNew) = await IconHelper.AddIcon(decodedIconUrl, queryString, name, _context, _mapper, setting, fd, cancellationToken).ConfigureAwait(false);
 
         return isNew;
     }
