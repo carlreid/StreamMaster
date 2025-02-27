e784fcf06511eaa604bea9216dfd69a8fd939af0
Carl Reid <kontakt@carlreid.dk>
2025-02-27 20:01:07 +0100

    fix: Layout now laying out

--- src/StreamMaster.UI/app/layout.tsx
+++ src/StreamMaster.UI/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import Provider from "./provider";
 import Sidebar from "../components/sidebar";
+import { Box, ClientOnly, Flex, Skeleton } from "@chakra-ui/react";
+import { ColorModeButton } from "../components/ui/color-mode";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -17,8 +19,17 @@ export default function RootLayout({
 			<head />
 			<body>
 				<Provider>
-					<Sidebar />
-					{children}
+					<Flex h="100vh" w="100%">
+						<Box pos="absolute" top="4" right="4">
+							<ClientOnly fallback={<Skeleton w="10" h="10" rounded="md" />}>
+								<ColorModeButton />
+							</ClientOnly>
+						</Box>
+						<Sidebar />
+						<Box flex="1" overflowY="auto">
+							{children}
+						</Box>
+					</Flex>
 				</Provider>
 			</body>
 		</html>
---
+++ src/StreamMaster.UI/app/streams/page.tsx
@@ -0,0 +1,21 @@
+import {
+	Box,
+	Button,
+	Checkbox,
+	ClientOnly,
+	HStack,
+	Heading,
+	Progress,
+	RadioGroup,
+	Skeleton,
+	VStack,
+} from "@chakra-ui/react";
+import Image from "next/image";
+
+export default async function StreamsPage() {
+	return (
+		<Box textAlign="center" fontSize="xl" pt="30vh">
+			Hello!
+		</Box>
+	);
+}
